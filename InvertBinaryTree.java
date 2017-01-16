b53bafb98eea078021d17df3b605a16771f587b8
akhilkateja123 <akhilkateja@gmail.com>
2017-01-16 14:14:25 -0600

    Update InvertBinaryTree.java
    Edit

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -4,24 +4,6 @@ import java.util.Stack;
 
 public class InvertBinaryTree {
 
-	public static void main(String[] args) {
- 
-		InvertBinaryTree obj = new InvertBinaryTree();
-		TreeNode root = new TreeNode(0);
-		TreeNode one = new TreeNode(1);
-		TreeNode two = new TreeNode(2);
-		TreeNode three = new TreeNode(3);
-		TreeNode four = new TreeNode(4);
-		TreeNode five = new TreeNode(5);
-		TreeNode six = new TreeNode(6);
-		root.left = one;
-		root.right = two;
-		one.left = three;
-		one.right = four;
-		two.left = five;
-		two.right = six;
-		System.out.println(obj.invertTree(root));
-	}
 
 	public TreeNode invertTree(TreeNode root) {
      
