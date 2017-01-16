b1511e5d629d84c5336caac1a0550b364cd4bc06
akhilkateja123 <akhilkateja@gmail.com>
2017-01-16 14:12:24 -0600

    Update SumLeftLeaves.java
    Edit

--- SumLeftLeaves.java
+++ SumLeftLeaves.java
@@ -4,20 +4,6 @@ import java.util.Stack;
 
 public class SumLeftLeaves {
 
-	public static void main(String[] args) {
-		SumLeftLeaves obj = new SumLeftLeaves();
-		TreeNode root = new TreeNode(1);
-		TreeNode one = new TreeNode(2);
-		TreeNode two = new TreeNode(3);
-		TreeNode three = new TreeNode(4);
-		TreeNode four = new TreeNode(5);
-		root.left = one;
-		root.right = two;
-		one.left = three;
-		one.right = four;
-		System.out.println(obj.sumOfLeftLeaves(root));
-	}
-
 	public int sumOfLeftLeaves(TreeNode root) {
 		int sum = 0;
 
