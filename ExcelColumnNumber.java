95a51d109e208e92425fa322ce956fa5fafed483
akhilkateja123 <akhilkateja@gmail.com>
2017-01-20 09:40:06 -0600

    Add files via upload
    More Code

---
+++ ExcelColumnNumber.java
@@ -0,0 +1,16 @@
+package com.leetcode.easy;
+
+public class ExcelColumnNumber {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int titleToNumber(String s) {
+      
+		
+		return 0;
+		
+	}
+
+}
