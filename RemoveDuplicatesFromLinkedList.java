df3c7200a2c90ea5f955fcff413991134a1fd223
akhilkateja123 <akhilkateja@gmail.com>
2017-01-27 09:34:40 -0600

    Update RemoveDuplicatesFromLinkedList.java
    More Code.

--- RemoveDuplicatesFromLinkedList.java
+++ RemoveDuplicatesFromLinkedList.java
@@ -11,32 +11,6 @@ class ListNode {
 
 public class RemoveDuplicatesFromLinkedList {
 
-	public static void main(String... args) {
-
-		RemoveDuplicatesFromLinkedList obj = new RemoveDuplicatesFromLinkedList();
-		ListNode head = new ListNode(0);
-		ListNode one = new ListNode(1);
-		ListNode two = new ListNode(1);
-		ListNode three = new ListNode(1);
-		ListNode four = new ListNode(2);
-		ListNode five = new ListNode(2);
-
-		head.next = one;
-		one.next = two;
-		two.next = three;
-		three.next = four;
-		three.next = five;
-		five.next = null;
-
-		ListNode newHead = obj.deleteDuplicates(head);
-		ListNode currentNode = newHead;
-		while (currentNode != null) {
-			System.out.println(currentNode.val + "-->");
-			currentNode = currentNode.next;
-		}
-
-	}
-
 	public ListNode deleteDuplicates(ListNode head) {
 
 		if (head == null || head.next == null)
