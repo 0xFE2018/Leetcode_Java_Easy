58edfbaa7ae399a5e96b305019d56bffd610b596
akhilkateja123 <akhilkateja@gmail.com>
2017-03-16 18:29:27 -0500

    Update MinDepthBinaryTree.java
    Edit

--- MinDepthBinaryTree.java
+++ MinDepthBinaryTree.java
@@ -29,24 +29,4 @@ public class MinDepthBinaryTree {
 		findDepth(node.right, depth);
 
 	}
-
-	public static void main(String... args) {
-		MinDepthBinaryTree obj = new MinDepthBinaryTree();
-
-		TreeNode root = new TreeNode(0);
-		TreeNode one = new TreeNode(1);
-		TreeNode two = new TreeNode(2);
-		TreeNode three = new TreeNode(3);
-		TreeNode four = new TreeNode(4);
-		//TreeNode five = new TreeNode(5);
-		//TreeNode six = new TreeNode(6);
-		root.left = one;
-		root.right = two;
-		one.left = three;
-		one.right = four;
-		//two.left = five;
-		//two.right = six;
-
-		System.out.println(obj.minDepth(root));
-	}
 }
