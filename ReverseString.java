58c21a5b93ea77a34abb341bda4736e84f8c8101
akhilkateja123 <akhilkateja@gmail.com>
2017-05-16 12:56:58 -0500

    More Practice Code.

--- ReverseString.java
+++ ReverseString.java
@@ -1,20 +1,32 @@
-package Easy;
+package com.leetcode.easy;
 
 public class ReverseString {
 
-	public String reverseString(String s) {
-	
-		char[] charArr = s.toCharArray();
-		int i = 0;
-		int j = charArr.length-1;
-		while (i < j) {
-			char temp = charArr[i];
-			charArr[i] = charArr[j];
-			charArr[j] = temp;
-			i++;
-			j--;
-		}
-		
-		return new String(charArr);
+	public static void main(String... args) {
+		ReverseString obj = new ReverseString();
+		System.out.println(obj.reverseWords("Let's take LeetCode contest"));
 	}
+
+	public String reverseWords(String s) {
+
+		StringBuilder builder = new StringBuilder();
+		String[] strArr = s.split(" ");
+		for (int k = 0; k < strArr.length; k++) {
+			char[] arr = strArr[k].toCharArray();
+			int i = 0, j = arr.length - 1;
+
+			while (i < j) {
+				char temp = arr[i];
+				arr[i++] = arr[j];
+				arr[j--] = temp;
+			}
+
+			if (!(k == 0))
+				builder.append(" ");
+			builder.append(new String(arr));
+		}
+
+		return builder.toString();
+	}
+
 }
